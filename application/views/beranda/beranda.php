c825f984db191398c6c13a597d773e99d6c97a9b
Ajenkris Kungkung <kristt26@gmail.com>
2021-12-03 18:17:20 +0900

    as

--- application/views/beranda/beranda.php
+++ application/views/beranda/beranda.php
@@ -1,35 +1,36 @@
 <section class="blog section-padding">
-<br><br><br>
-   <div class="container">
-    <center>
-      <h4>Berita</h4>
-    <div class="row">
-      <div class="col-lg-6">
+    <br><br><br>
+    <div class="container">
+        <center>
+            <h4>Berita</h4>
+            <div class="row">
+                <div class="col-lg-6">
 
-      <?php foreach($berita_terakhir as $data) : ?>
-        <div class="card" style="width: 42rem;  border-radius: 10px;">
-          <img style="height:300px;" class="card-img-top" src="<?= base_url('assets/img/berita/') . $data['gambar'] ?>" alt="Card image cap">
-          <a href="#"><?= $data['judul'] ?></a>
-            <p class="card-text"><small><?= tgl_indo($data['tanggal']); ?></small></p>
-        </div>
-      <?php endforeach; ?>
-      </div>
-      <div class="col-lg-6">
-        <?php foreach($berita as $data) : ?>
-          <div class="card mb-3" style="width: 15rem; border-radius: 10px;">
-            <img class="card-img-top" src="<?= base_url('assets/img/berita/') . $data['gambar'] ?>" alt="Card image cap">
-            <div class="card-footer">
-          	  <a href="#" class="text-justify"><?= $data['judul'] ?></a>
-            	<p class="card-text"><small><?= tgl_indo($data['tanggal']); ?></small></p>
+                    <?php foreach($berita_terakhir as $data) : ?>
+                    <div class="card" style="width: 42rem;  border-radius: 10px;">
+                        <img style="height:300px;" class="card-img-top"
+                            src="<?= base_url('assets/img/berita/') . $data['gambar'] ?>" alt="Card image cap">
+                        <a href="#"><?= $data['judul'] ?></a>
+                        <p class="card-text"><small><?= tgl_indo($data['tanggal']); ?></small></p>
+                        <div class="card-body text-left">
+                            <?= $data['isi_berita'] ?>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+                <div class="col-lg-6">
+                    <?php foreach($berita as $data) : ?>
+                    <div class="card mb-3" style="width: 15rem; border-radius: 10px;">
+                        <img class="card-img-top" src="<?= base_url('assets/img/berita/') . $data['gambar'] ?>"
+                            alt="Card image cap">
+                        <div class="card-footer">
+                            <a href="#" class="text-justify"><?= $data['judul'] ?></a>
+                            <p class="card-text"><small><?= tgl_indo($data['tanggal']); ?></small></p>
+                        </div>
+                    </div>
 
+                    <?php endforeach; ?>
+                </div>
             </div>
-          </div>
-
-        <?php endforeach; ?>
-      </div>
+        </center>
     </div>
-    </center>
-  </div>
-                   
-
-                 
